7e226d9127aa22246e55c91893f4e48ce01a6f2b
Pratik Karanjit <pratikkaranjit@gmail.com>
2024-02-24 11:13:20 +0545

    add input field

--- react-redux-app/src/Game.jsx
+++ react-redux-app/src/Game.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 
 const Game = () => {
   let [value, setValue] = useState();
-  let isValid = value >= 0 && value <= 11;
+  let [message, setMessage] = useState("");
+  let isValid = value >= 0;
 
   return (
     <div>
@@ -16,7 +17,7 @@ const Game = () => {
           }}
         ></input>
       </label>
-      <p>{isValid ? "The number is valid." : "The number is not valid."}</p>;
+      <p>{isValid ? "The number is valid." : "The number is not valid."}</p>
       <button>Submit</button>
     </div>
   );
